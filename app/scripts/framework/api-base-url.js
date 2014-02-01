6c18848db10666a60dbd15ce2b4fd18c5b9a87e1
Kenneth Lynne <kenneth.lynne@gmail.com>
2014-02-01 20:33:52 +0100

    update

--- app/scripts/framework/api-base-url.js
+++ app/scripts/framework/api-base-url.js
@@ -1,3 +1,3 @@
-angular.module('ndc').factory('BaseUrl', function (Config) {
-    return (Config.API.protocol + '://' + Config.API.host + ':' + Config.API.port + '/');
+angular.module('ndc').factory('APIBaseUrl', function (Config) {
+    return (Config.API.protocol + '://' + Config.API.host + ':' + Config.API.port + Config.API.path + '/');
 });
