8a534ea650764413dd087d0abafdbf1409e27c2d
Kristoffer Sundmyhr <kristoffer@sundmyhr.no>
2014-02-16 00:18:59 +0100

    Active sidenav links, dummy content agenda (not designed)

--- app/components/navbar/navbar.js
+++ app/components/navbar/navbar.js
@@ -10,6 +10,18 @@ angular.module('ndc.components')
         $scope.isAuthenticated = function () {
             return authentication.isAuthenticated();
         };
+
+        $scope.getClass = function(path) {
+            var cur_path = $location.path().substr(0, path.length);
+            if (cur_path == path) {
+                if($location.path().substr(0).length > 1 && path.length == 1 )
+                    return "";
+                else
+                    return "active";
+            } else {
+                return "";
+            }
+        }
     })
     .component('navbar', function () {
         return {
