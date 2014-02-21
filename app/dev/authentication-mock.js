db700c58747d861138daa5cbf24ee3f98ddb4397
Kenneth Lynne <kenneth.lynne@gmail.com>
2014-02-21 01:37:01 +0100

    log in with whatever

--- app/dev/authentication-mock.js
+++ app/dev/authentication-mock.js
@@ -22,7 +22,8 @@ angular.module('ndc')
         $httpBackend.whenPOST(baseUrl).respond(function(method, url, data, headers) {
             $log.log('Intercepted POST to token', data);
 
-            if(data == 'grant_type=password&username=ali@g.com&password=pw' && headers['Accept'] == 'application/json, text/plain, */*')
+            //data == 'grant_type=password&username=ali@g.com&password=pw' && headers['Accept'] == 'application/json, text/plain, */*'
+            if(true)
             {
                 var response = {
                     "access_token":"take-on-me",
