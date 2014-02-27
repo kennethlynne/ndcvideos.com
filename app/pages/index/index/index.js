775c0e63fa5002cc83e9b0dec73c4d265eb4a4a0
Are Almaas <arealmaas@gmail.com>
2014-02-27 08:59:21 +0100

    simple POC for Tagsearch

--- app/pages/index/index/index.js
+++ app/pages/index/index/index.js
@@ -3,11 +3,34 @@
 angular.module('ndc')
     .config(function ($stateProvider, stateFactory) {
         $stateProvider.state('index', stateFactory('Index', {
-            url:'/'
+            url:'/?tags'
         }));
     })
-    .controller('IndexCtrl', function ($scope, VideoRepository) {
-        VideoRepository.getAll().then(function (videos) {
-            $scope.videos = videos;
-        });
+    .controller('IndexCtrl', function ($scope, VideoRepository, $stateParams, _) {
+
+
+        if($stateParams.tags != null)
+        {
+            //TODO:
+            //VideosRepository.getAllByTags();
+            var tags = $stateParams.tags.split(',');
+            var tagArray = [];
+            for(var i = 0;i < tags.length;i++)
+            {
+                tagArray[i] = {title:tags[i]};
+            }
+
+            VideoRepository.getAll().then(function (videos) {
+                console.log(videos);
+                $scope.videos = _.where(videos, {tags:tagArray});
+            });
+
+        }
+        else
+        {
+            VideoRepository.getAll().then(function (videos) {
+                $scope.videos = videos;
+            });
+        }
+
     });
