ab49d1d838997d5ed7b487969dec60cbc00d51a3
Kenneth Lynne <kenneth.lynne@gmail.com>
2014-03-08 13:21:58 +0100

    implement basic user creation logic

--- app/pages/admin/index/admin-controller.js
+++ app/pages/admin/index/admin-controller.js
@@ -3,10 +3,47 @@
 angular.module('ndc')
     .config(function ($stateProvider, stateFactory) {
         $stateProvider.state('admin', stateFactory('Admin', {
-            url:'/admin',
+            url: '/admin',
             templateUrl: 'pages/admin/index/main-view.html'
         }));
     })
-    .controller('AdminCtrl', function ($scope, init) {
-        $scope.data = init;
+    .controller('AdminCtrl', function ($scope, UserRepository, VideoRepository) {
+
+        $scope.users = [];
+        UserRepository.getAll().then(function (users) {
+            $scope.users = users;
+        });
+
+        $scope.videos = [];
+        VideoRepository.getAll().then(function (videos) {
+            $scope.videos = videos;
+        });
+
+        $scope.createUser = function () {
+            $scope.isCreatingNewUser = true;
+        };
+
+        $scope.resetNewUser = function () {
+            $scope.isCreatingNewUser = false;
+            $scope.newUser = {};
+        };
+
+        $scope.saveUser = function (user) {
+            $scope.isCreatingNewUser = false;
+
+            var User = UserRepository.create(user);
+            $scope.users.push(user);
+
+            User.$save()
+                .then(function (user) {
+                    $scope.newUser = {};
+                })
+                .catch(function () {
+                    $scope.isCreatingNewUser = true;
+                    $scope.users.splice($scope.users.indexOf(User), 1);
+                    //TODO: More user friendly feedback
+                    alert('Klarte ikke lagre brukeren');
+                });
+
+        };
     });
