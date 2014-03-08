5f83f8bdc11e52f790062edb07511d5fd6adb172
Kristoffer Sundmyhr <kristoffer@sundmyhr.no>
2014-03-08 15:15:41 +0100

    Add admin interface, responsive admin tables, minor ui fixes closes #34

--- app/pages/admin/index/admin-controller.js
+++ app/pages/admin/index/admin-controller.js
@@ -9,4 +9,17 @@ angular.module('ndc')
     })
     .controller('AdminCtrl', function ($scope, init) {
         $scope.data = init;
+
+        $(".tabs-navigation li").eq(0).addClass("active");
+        $(".tabs-content li").eq(0).addClass("active");
+
+        $(".tabs-navigation li").click(function() {
+        	var id = $(this).attr("id");
+
+			$(".tabs-navigation li").removeClass("active");
+        	$(this).addClass("active");
+
+        	$(".tabs-content li").removeClass("active");
+        	$(".tabs-content li#" + id + "-content").addClass("active");
+        });
     });
