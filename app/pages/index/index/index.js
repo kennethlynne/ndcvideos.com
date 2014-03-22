95867153c9abca3955aac4e971f1b006b525c309
Are Almaas <arealmaas@gmail.com>
2014-03-22 14:02:45 +0100

    Changed the search logic

--- app/pages/index/index/index.js
+++ app/pages/index/index/index.js
@@ -3,72 +3,91 @@
 angular.module('ndc')
     .config(function ($stateProvider, stateFactory) {
         $stateProvider.state('index', stateFactory('Index', {
-            url:'/?tags',
-            reloadOnSearch:false
+            url: '/?q',
+            reloadOnSearch: false
         }));
     })
     .controller('IndexCtrl', function ($scope, CurrentUser, TagRepository, VideoRepository, $stateParams, $location, _, TagModel) {
 
 
-
-        $scope.tags = []; //This variable holds selected tags
-
         $scope.User = CurrentUser;
 
-        $scope.select2Options = {
-            multiple: true,
-            query: function (query) {
-                TagRepository.search(query.term).then(function (data) {
-                    query.callback({results: data});
-                });
-            }
-        };
+        $scope.search = _.throttle(function (query) {
 
-        if($stateParams.tags != null)
-        {
-            setVideosByTags($stateParams.tags);
-            $scope.tags = _.map($stateParams.tags.split(','),function (item)
-            {
-                return new TagModel({text:item});
-            });
-        }
-        else
-        {
-            setAllVideos();
-        }
+            //update query parameters and set videos by query
+            $location.search('q', query ? query : null);
 
-        $scope.filter = function ()
-        {
-           if($scope.tags.length > 0)
-           {
-               var tags = _.map($scope.tags, function (item)
-               {
-                   return item.text;
-               }).join(',');
+            getVideos(query);
 
-               $location.search('tags', tags);
-               setVideosByTags(tags);
-           }
-            else
-           {
-               //To reset the URL params
-               delete $location.$$search.tags;
-               $location.$$compose();
-               setAllVideos();
-           }
-        }
+        }, 500, {'leading': false, 'trailing': true});
 
-        function setAllVideos(){
-            VideoRepository.getAll().then(function (videos) {
+        var getVideos = function (query) {
+            VideoRepository.where(query).then(function (videos) {
+                $scope.errormsg = videos.length<1?'No videos :/':null;
+
                 $scope.videos = videos;
             });
         };
 
+        $scope.query = $stateParams.q;
 
-        function setVideosByTags(tags)
-        {
-            VideoRepository.getByTags(tags).then(function (videos) {
-                $scope.videos = videos;
-            });
-        };
+        getVideos($stateParams.q);
+
+
+//        $scope.select2Options = {
+//            multiple: true,
+//            query: function (query) {
+//                TagRepository.search(query.term).then(function (data) {
+//                    query.callback({results: data});
+//                });
+//            }
+//        };
+//
+//        if($stateParams.q != null)
+//        {
+//            setVideosByTags($stateParams.tags);
+//            $scope.tags = _.map($stateParams.tags.split(','),function (item)
+//            {
+//                return new TagModel({text:item});
+//            });
+//        }
+//        else
+//        {
+//            setAllVideos();
+//        }
+//
+//        $scope.filter = function ()
+//        {
+//           if($scope.tags.length > 0)
+//           {
+//               var tags = _.map($scope.tags, function (item)
+//               {
+//                   return item.text;
+//               }).join(',');
+//
+//               $location.search('tags', tags);
+//               setVideosByTags(tags);
+//           }
+//            else
+//           {
+//               //To reset the URL params
+//               delete $location.$$search.tags;
+//               $location.$$compose();
+//               setAllVideos();
+//           }
+//        }
+//
+//        function setAllVideos(){
+//            VideoRepository.getAll().then(function (videos) {
+//                $scope.videos = videos;
+//            });
+//        };
+//
+//
+//        function setVideosByTags(tags)
+//        {
+//            VideoRepository.getByTags(tags).then(function (videos) {
+//                $scope.videos = videos;
+//            });
+//        };
     });
