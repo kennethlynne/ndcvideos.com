ac3b49059703141d0fec721b9b8441f5067c9d1d
Kenneth Lynne <kenneth.lynne@gmail.com>
2014-03-22 15:53:00 +0100

    Auto-generated build from v0.0.0

--- config/routes.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('ndc')
-    .config(function ($urlRouterProvider) {
-        $urlRouterProvider.when('', '/');
-        $urlRouterProvider.otherwise("/error?code=404");
-    });
--- scripts/repositories/user-repository.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('ndc')
-    .factory('UserRepository', function ($injector, UserModel) {
-        var BaseRepository = $injector.get('BaseRepository');
-        return new BaseRepository({name: 'UserRepository', model: UserModel});
-    });
