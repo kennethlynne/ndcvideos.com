c76f7c1908979263b3d456768a6d23836ae81394
Kenneth Lynne <kenneth.lynne@gmail.com>
2014-03-22 12:41:42 +0100

    better tests on state-history

--- test/unit/spec/services/state-history.js
+++ test/unit/spec/services/state-history.js
@@ -2,11 +2,17 @@
 
 describe('Service: stateHistory', function () {
 
-    var stateHistory;
+    var stateHistory, $state;
 
     beforeEach(function () {
 
-        module('ndc');
+        $state = {
+            go: jasmine.createSpy('$state.go')
+        };
+
+        module('ndc', function ($provide) {
+            $provide.value('$state', $state);
+        });
 
         inject(function (_stateHistory_) {
             stateHistory = _stateHistory_;
@@ -14,9 +20,28 @@ describe('Service: stateHistory', function () {
 
     });
 
-
     it('should do something', function () {
         expect(!!stateHistory).toBe(true);
+    });
+
+    it('should remember a state', function () {
+        stateHistory.go('state', {awesome: true})
+        expect(stateHistory.pop()).toEqual({name: 'state', params: {awesome: true}})
+    });
+
+    it('should return a default state if history is empty', function () {
+        expect(stateHistory.history.length).toBe(0);
+        expect(stateHistory.pop()).toEqual({name: 'index', params: {}})
+    });
+
+    it('should go back', function () {
+        var fixed = {name: 'state', params: {}};
+        stateHistory.pop = function () {
+            return fixed
+        };
+        stateHistory.back();
+
+        expect($state.go).toHaveBeenCalledWith(fixed.name, fixed.params);
     });
 
 });
