d6f158f7904a776e056ebd5e55f16fc69cb2df1a
Kenneth Lynne <kenneth.lynne@gmail.com>
2014-04-28 22:10:28 +0200

    fix save in video mocks

--- app/scripts/models/video.js
+++ app/scripts/models/video.js
@@ -1,32 +1,32 @@
 'use strict';
 
 angular.module('ndc')
-    .factory('VideoModel', function ($q, $http, $rootScope, BaseModel, APIBaseUrl, $injector, timeFilter) {
+  .factory('VideoModel', function ($q, $http, $rootScope, BaseModel, APIBaseUrl, $injector, timeFilter) {
 
-        var collectionUrl = 'videos';
+    var collectionUrl = 'videos';
 
-        function VideoModel(data) {
-            data = data || {};
-            data.url = APIBaseUrl + collectionUrl;
+    function VideoModel(data) {
+      data = data || {};
+      data.url = APIBaseUrl + collectionUrl;
 
-            data.$durationInHHMMSS = timeFilter(data.duration);
+      data.$durationInHHMMSS = timeFilter(data.duration);
 
-            BaseModel.call(this,data);
-        }
+      BaseModel.call(this, data);
+    }
 
-        VideoModel.$settings = {url: APIBaseUrl + collectionUrl};
-        VideoModel.prototype = Object.create(BaseModel.prototype);
+    VideoModel.$settings = {url: APIBaseUrl + collectionUrl};
+    VideoModel.prototype = Object.create(BaseModel.prototype);
 
-        //Decorate save to attach this item to the Repository on successful save
-        var _$save = VideoModel.prototype.$save;
-        VideoModel.prototype.$save = function () {
-            var Video = this;
-            return _$save.apply(this, arguments).then(function (response) {
-                var Repository = $injector.get('VideoRepository');
-                Repository.attach(Video);
-                return response;
-            });
-        };
+    //Decorate save to attach this item to the Repository on successful save
+    var _$save = VideoModel.prototype.$save;
+    VideoModel.prototype.$save = function () {
+      var Video = this;
+      return _$save.apply(this, arguments).then(function (response) {
+        var Repository = $injector.get('VideoRepository');
+        Repository.attach(Video);
+        return response;
+      });
+    };
 
-        return VideoModel;
-    });
+    return VideoModel;
+  });
