d2c7f3dd1af426cd3ac28e33c146dbc82ade323d
Kenneth Lynne <kenneth.lynne@gmail.com>
2014-05-01 15:05:23 +0200

    clean up logout sidebar item

--- test/unit/spec/components/logout-sidebar-item.js
+++ test/unit/spec/components/logout-sidebar-item.js
@@ -3,15 +3,21 @@
 describe('Component: logoutSidebarItemComponent', function () {
 
   describe('Directive: logoutSidebarItemComponent', function () {
-    var element, scope, $compile;
+    var element, scope, $compile, authentication;
 
     beforeEach(function () {
 
-      module('ndc');
+      module('ndc', function ($provide) {
+        $provide.decorator('authentication', function ($delegate) {
+          $delegate.logout = jasmine.createSpy('authentication.logout');
+          return $delegate;
+        });
+      });
 
-      inject(function ($rootScope, _$compile_) {
+      inject(function ($rootScope, _$compile_, _authentication_) {
         scope = $rootScope.$new();
         $compile = _$compile_;
+        authentication = _authentication_;
       });
 
     });
@@ -23,6 +29,13 @@ describe('Component: logoutSidebarItemComponent', function () {
       expect(element).toHaveClass('logout-sidebar-item-component');
     });
 
+    it('should log the user out', function () {
+      element = angular.element('<logout-sidebar-item-component></logout-sidebar-item-component>');
+      element = $compile(element)(scope);
+      scope.$digest();
+      element.click();
+      expect(authentication.logout).toHaveBeenCalled();
+    });
   });
 
   describe('Controller: logoutSidebarItemComponentCtrl', function () {
