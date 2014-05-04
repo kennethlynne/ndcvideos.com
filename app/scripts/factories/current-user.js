dd4b97bf7a8bbf6d8e66a5a84752090679f028e9
Kenneth Lynne <kenneth.lynne@gmail.com>
2014-05-04 18:25:42 +0200

    fix indentation

--- app/scripts/factories/current-user.js
+++ app/scripts/factories/current-user.js
@@ -1,55 +1,54 @@
 'use strict';
 
 angular.module('ndc')
-    .factory('CurrentUser', function ($log, UserModel, _, $localStorage, accessControl) {
+  .factory('CurrentUser', function ($log, UserModel, _, $localStorage, accessControl) {
 
-        var storage = $localStorage['CurrentUser'] = $localStorage['CurrentUser'] || {};
-        var _user = storage.user = storage.user || {};
+    var storage = $localStorage['CurrentUser'] = $localStorage['CurrentUser'] || {};
+    var _user = storage.user = storage.user || {};
 
-        var _set = function (user) {
-                if(!(user instanceof UserModel)) $log.error("Expected UserModel when initializing CurrentUser");
+    var _set = function (user) {
+        if (!(user instanceof UserModel)) $log.error("Expected UserModel when initializing CurrentUser");
 
-                _setRoles(user.roles);
-                _setPermissions(user.permissions);
+        _setRoles(user.roles);
+        _setPermissions(user.permissions);
 
-                angular.copy(user, _user);
-            },
-            _unset = function () {
-                accessControl.unset();
-                angular.copy({}, _user);
-            },
-            _get = function ()
-            {
-                return _user;
-            },
-            _is = function (role) {
-                return accessControl.is(role);
-            },
-            _setRoles = function (roles) {
-                accessControl.setRoles(roles);
-            },
-            _getRoles = function () {
-                return accessControl.getRoles();
-            },
-            _setPermissions = function (permissions) {
-                accessControl.setPermissions(permissions);
-            },
-            _getPermissions = function () {
-                return accessControl.getPermissions();
-            },
-            _can = function (thing) {
-                return accessControl.can(thing);
-            };
+        angular.copy(user, _user);
+      },
+      _unset = function () {
+        accessControl.unset();
+        angular.copy({}, _user);
+      },
+      _get = function () {
+        return _user;
+      },
+      _is = function (role) {
+        return accessControl.is(role);
+      },
+      _setRoles = function (roles) {
+        accessControl.setRoles(roles);
+      },
+      _getRoles = function () {
+        return accessControl.getRoles();
+      },
+      _setPermissions = function (permissions) {
+        accessControl.setPermissions(permissions);
+      },
+      _getPermissions = function () {
+        return accessControl.getPermissions();
+      },
+      _can = function (thing) {
+        return accessControl.can(thing);
+      };
 
-        return {
-            set: _set,
-            unset: _unset,
-            get:_get,
-            can: _can,
-            is: _is,
-            setRoles: _setRoles,
-            getRoles: _getRoles,
-            setPermissions: _setPermissions,
-            getPermissions: _getPermissions
-        }
-    });
+    return {
+      set: _set,
+      unset: _unset,
+      get: _get,
+      can: _can,
+      is: _is,
+      setRoles: _setRoles,
+      getRoles: _getRoles,
+      setPermissions: _setPermissions,
+      getPermissions: _getPermissions
+    }
+  });
