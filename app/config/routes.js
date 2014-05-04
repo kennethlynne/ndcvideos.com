46e448c0fade22ba2e602fe6df837a20d679ba2a
Kenneth Lynne <kenneth.lynne@gmail.com>
2014-05-04 13:29:55 +0200

    remove trailing slashes from url closes #122

--- app/config/routes.js
+++ app/config/routes.js
@@ -4,5 +4,14 @@ angular.module('ndc')
   .config(function ($urlRouterProvider) {
     $urlRouterProvider.when('', '/app/videos');
     $urlRouterProvider.when('/', '/app/videos');
-    $urlRouterProvider.otherwise("/error?code=404");
+    $urlRouterProvider.otherwise('/error?code=404');
+
+    //Remove trailing slashes
+    $urlRouterProvider.rule(function (injector, location) {
+      var origPath = location.path(),
+        normalizedPath = origPath.replace(/\/$/, '');
+      if (origPath !== normalizedPath) {
+        location.replace().path(normalizedPath);
+      }
+    });
   });
