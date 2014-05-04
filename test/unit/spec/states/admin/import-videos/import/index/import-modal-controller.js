512a1711d96c6ec88b36fdf7558bb2de4c3bbb4b
Are Almaas <arealmaas@gmail.com>
2014-05-04 13:07:08 +0200

    fix tests

--- test/unit/spec/states/admin/import-videos/import/index/import-modal-controller.js
+++ test/unit/spec/states/admin/import-videos/import/index/import-modal-controller.js
@@ -2,18 +2,40 @@
 
 describe('Controller(/admin/import-videos/import): ImportVideoModalCtrl', function () {
 
-  var ImportVideoModalCtrl, scope;
+  var ImportVideoModalCtrl, scope, vimeoAPI, deferred, promise, $rootScope, $state = {params:{id:1}};
 
   beforeEach(function () {
 
+    function getPromise() {
+      return promise;
+    }
+
+    vimeoAPI = {
+      getVideoById: jasmine.createSpy('vimeoAPI.getVideoById').and.callFake(getPromise)
+    };
+
     module('ndc');
 
-    inject(function ($controller, $rootScope) {
+    inject(function ($controller, _$rootScope_, $q) {
+      deferred = $q.defer();
+      promise = deferred.promise;
+      $rootScope = _$rootScope_;
       scope = $rootScope.$new();
+
       ImportVideoModalCtrl = $controller('ImportVideoModalCtrl', {
-        $scope: scope
+        $scope: scope,
+        vimeoAPI: vimeoAPI
       });
     });
   });
+
+  it('should get a video from vimeo', function () {
+    var video = {id:1, name:'ragnar'};
+    deferred.resolve(video);
+    $rootScope.$digest();
+    console.log(scope.vimeoVideo);
+    expect(scope.vimeoVideo).toBe(video);
+  });
+
 
 });
