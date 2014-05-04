1f61d6db6787b2b2a85bbabcf83e29b659e9e565
Are Almaas <arealmaas@gmail.com>
2014-05-04 12:29:22 +0200

    erro handling

--- app/states/welcome/index/welcome-controller.js
+++ app/states/welcome/index/welcome-controller.js
@@ -6,10 +6,11 @@ angular.module('ndc')
       url: '/welcome?verificationToken',
       templateUrl: 'states/welcome/index/main-view.html',
       resolve: {
-        userToBeVerified: ['UserRepository', '$stateParams', '$log', function (UserRepository, $stateParams, $log) {
+        userToBeVerified: ['UserRepository', '$stateParams', '$log', function (UserRepository, $stateParams, $log, $state) {
           return UserRepository.getByVerificationToken($stateParams.verificationToken)
             .catch(function (err) {
               $log.error(err);
+              $state.go('error', {code:404});
             });
         }]
       }
@@ -25,10 +26,17 @@ angular.module('ndc')
             .then(function (response) {
               $state.go('videos');
               return response;
+            })
+            .catch(function(err){
+              //Not able to authenticate the user
+              $log.log(err);
+              $state.go('error', {code:403});
             });
         })
         .catch(function (err) {
+          //Not able to verify the user
           $log.error(err);
+          $state.go('error', {code:403});
         });
     };
   });
