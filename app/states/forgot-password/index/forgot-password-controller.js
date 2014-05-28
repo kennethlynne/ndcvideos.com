574c9121e27a41dab869db56b0313a01b350c200
Kenneth Lynne <kenneth.lynne@gmail.com>
2014-05-28 21:22:59 +0200

    more work on forgot-password

--- app/states/forgot-password/index/forgot-password-controller.js
+++ app/states/forgot-password/index/forgot-password-controller.js
@@ -3,10 +3,23 @@
 angular.module('ndc')
   .config(function ($stateProvider, stateFactory) {
     $stateProvider.state('forgot-password', stateFactory('Forgotpassword', {
-      url: '/forgot-password',
+      url: '/forgot-password?mailSent&token',
       templateUrl: 'states/forgot-password/index/main-view.html'
     }));
   })
-  .controller('ForgotpasswordCtrl', function ($scope) {
-    $scope.foo = 'bar';
+  .controller('ForgotpasswordCtrl', function ($scope, UserRepository, $state, $log, $stateParams) {
+
+    $scope.mailSent = !!($stateParams.mailSent || $stateParams.token);
+    $scope.token = $stateParams.token;
+
+    $scope.sendMail = function () {
+      UserRepository.resetPasswordFor($scope.email)
+        .then(function () {
+          $state.go('forgot-password', {mailSent: true});
+        })
+        .catch(function (err) {
+          $log.error(err);
+        });
+    }
+
   });
