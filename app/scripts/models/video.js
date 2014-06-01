4d2e6cd2682d6541fe8a3f6dc2bfcda9056fca18
Kenneth Lynne <kenneth.lynne@gmail.com>
2014-06-01 18:46:36 +0200

    don't die on erronously formatted dates

--- app/scripts/models/video.js
+++ app/scripts/models/video.js
@@ -11,9 +11,11 @@ angular.module('ndc')
 
       data.$durationInHHMMSS = timeFilter(data.duration);
 
-      data.upload_date = data.upload_date ? new Date(data.upload_date) : new Date();
-      data.createdAt = data.createdAt ? new Date(data.createdAt) : new Date();
-      data.updatedAt = data.updatedAt ? new Date(data.updatedAt) : new Date();
+      var dateRegex = /(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d)/;
+
+      data.upload_date = data.upload_date && dateRegex.test(data.upload_date) ? new Date(data.upload_date) : new Date();
+      data.createdAt = data.createdAt && dateRegex.test(data.createdAt) ? new Date(data.createdAt) : new Date();
+      data.updatedAt = data.updatedAt && dateRegex.test(data.updatedAt) ? new Date(data.updatedAt) : new Date();
 
       BaseModel.call(this, data);
     }
