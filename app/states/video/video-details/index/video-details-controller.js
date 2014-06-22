330bdef3073710f10fca2072791c957500cb33bd
Kenneth Lynne <kenneth.lynne@gmail.com>
2014-06-22 22:58:58 +0200

    un-modalify video details

--- app/states/video/video-details/index/video-details-controller.js
+++ app/states/video/video-details/index/video-details-controller.js
@@ -3,25 +3,9 @@
 angular.module('ndc')
   .config(function ($stateProvider, stateFactory) {
     $stateProvider.state('videoDetails', stateFactory('Videodetails', {
-      url: '/{id}',
-      resolve: {
-        scrollLock: ['scrollLock', function (sl) {
-          return sl;
-        }]
-      },
-      views: {
-        'slideOut@app': {
-          templateUrl: 'states/video/video-details/index/slide-out-view.html',
-          controller: 'VideodetailsCtrl'
-        }
-      },
-      onEnter: ['scrollLock', function (sl) {
-        sl.enable();
-      }],
-      onExit: ['scrollLock', function (sl) {
-        sl.disable();
-      }],
-      parent: 'videos'
+      url: '/video/{id}',
+      templateUrl: 'states/video/video-details/index/main-view.html',
+      parent: 'app'
     }));
   })
   .controller('VideodetailsCtrl', function ($scope, $stateParams, $state, VideoRepository, stateHistory) {
